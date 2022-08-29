a7274a42743f617a0d8d22f9df07ed5ae9c8f1cb
Jorge Trindade <86167567+JorgeTrindadeDev@users.noreply.github.com>
2022-08-28 22:27:32 -0300

    while

---
+++ Python_Exercicios.py
@@ -0,0 +1,47 @@
+'''num=input("Digite um numero: ")
+num=float(num)
+
+print(f'O numero digitado e: {num} ')
+print(type(num), num)'''
+
+'''nomes=['Ana','Carlos','Daiane','Fernando','Maria']
+print(len(nomes))
+print(nomes[3])'''
+'''num1=input('Insira um valor: ')
+num1=int(num1)
+num2=input("Insira o segundo valor: ")
+num2=int(num2)
+soma=num1+num2
+print(soma)
+print(num1-num2)
+print(num1/num2)
+print(num1*num2)
+#elevando a potencializacao
+print(num1 ** 8)'''
+'''num1= 100
+num2=89
+print(num1>num2)
+print(num1==num2)
+print(num1!=num2)
+print(num1 <=100 and num2<=100)
+print(num1 and num2 <=100)
+print(num1 >=100 or num2 >= 100)'''
+'''num1=100
+lista1=[10,100,1000,10000,100000]
+print(num1 in lista1)'''
+'''num1=int(input('Digite o primeiro numero: '))
+num2=int(input('Digite o segundo numero: '))
+
+if num1 > num2:
+    print('O primeiro numero e maior que o segundo!!!')
+else:
+    print('O segundo numero e o maior')'''
+'''num1=int(input('Digite um numero: '))
+if num1 %2 == 0:
+    print(f'{num1} Par')
+else:
+    print(f'{num1} Impar')'''
+var=0
+while var < 10:
+    print(var)
+    var=var+1
